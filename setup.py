6f872d659540b03fdffbbebee797e8992d33d682
Maurits van Rees <maurits@vanrees.org>
2013-09-23 18:05:45 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ version = open(os.path.join("collective",
 setup(name='collective.multimodeview',
       version=version,
       description="Simple package to manage views with multiple modes.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("collective",
-                                         "multimodeview",
-                                         "HISTORY.txt")).read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open(os.path.join("collective",
+                                          "multimodeview",
+                                          "HISTORY.txt")).read()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          ],
+      keywords='multimode view',
+      author='Zest Software',
+      author_email='info@zestsoftware.nl',
+      url='https://github.com/zestsoftware/collective.multimodeview',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
@@ -29,7 +29,6 @@ setup(name='collective.multimodeview',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
@@ -37,6 +36,4 @@ setup(name='collective.multimodeview',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
